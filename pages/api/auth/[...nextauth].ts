35a317dd2727c8f515610697fd5f0c58645703b5
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2021-06-27 01:34:51 +0530

    done

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,19 @@
-import { NextApiHandler } from 'next'
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
-import Adapters from 'next-auth/adapters'
-import prisma from '../../../lib/prisma'
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import prisma from '../../../lib/prisma';
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
-export default authHandler
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
 
 const options = {
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  adapter: Adapters.Prisma.Adapter({ prisma }),
-  secret: process.env.SECRET,
-}
+	providers: [
+		Providers.Facebook({
+			clientId: process.env.FACEBOOK_ID,
+			clientSecret: process.env.FACEBOOK_SECRET,
+		}),
+	],
+	adapter: Adapters.Prisma.Adapter({ prisma }),
+	secret: process.env.SECRET,
+};
